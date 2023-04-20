e30efc630136f34cab2c8215627a2c2e4bdf9801
joe <centeno.joe@gmail.com>
2023-04-20 09:24:19 -0700

    Finished Exam

--- LaunchMod1Week5Assessment/User.cs
+++ LaunchMod1Week5Assessment/User.cs
@@ -4,60 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace User
+namespace UserSpace
 {
-    public class User
-    {
-        public string Name { get; private set; }
-        public string Email { get; private set; }
-        private string Password { get; set; }
-        public bool IsLoggedIn { get; set; }
+	public class User
+	{
+		public string Name { get; private set; }
+		public string Email { get; private set; }
+		private string Password { get; set; }
+		public bool IsLoggedIn { get; set; }
 
-        public User(string name, string email)
-        {
-            Name = name;
-            Email = email;
-            IsLoggedIn = false;
+		public User(string name, string email)
+		{
+			Name = name;
+			Email = email;
+			IsLoggedIn = false;
+		}
+
+		public bool IsSetupComplete()
+		{
+			if (Name != null && Email != null && Password != null)
+			{
+			   return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+        // refactored method bellow so that it returns the string "Password Created" instead of saving it to a var that dose not get used and gose out of scpoe once the method is done
+        public string CreatePassword(string email, string password)
+		{
+			if (email == Email)
+			{
+				Password = password;
+			}
+
+			if (Password != null)
+			{	
+				return "Password Created";
+			}
+
+            return "Password Not-Created";
         }
 
-        public bool IsSetupComplete()
-        {
-            if (Name != null && Email != null && Password != null)
-            {
-               return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+		public string LogIn(string password)
+		{
+			if (password == Password)
+			{
+				IsLoggedIn = true;
+				return "Logged In";
+			}
+			return "Not Logged In";
+		}
 
-        public void CreatePassword(string email, string password)
-        {
-            if (email == Email)
-            {
-                Password = password;
+        // refactored so this method would return a string instead of being void to give more feedback to the user that the action took place. I also made it so it would check to make sure the user was loged in before setting the IsLoggedIn bool to false. 
+        public string LogOut()
+		{
+			if (IsLoggedIn)
+			{
+                IsLoggedIn = false;
+                return "Logged Out";
             }
 
-            if (Password != null)
-            {
-                var confirmation = "Password Created";
-            }
-        }
-
-        public string LogIn(string password)
-        {
-            if (password == Password)
-            {
-                IsLoggedIn = true;
-                return "Logged In";
-            }
-            return "Not Logged In";
-        }
-
-        public void LogOut()
-        {
-            IsLoggedIn = false;
-        }
-    }
+			return "Please Login first before logging out";
+		}
+	}
 }
--- UserTests/UserTest.cs
+++ UserTests/UserTest.cs
@@ -1,11 +1,71 @@
+/*
+ * The class works, but it needs some additional effort to be great! One thing that is missing is tests; your job is to write the tests that will confirm the functionality of the class. Write one test for the constructor, and at least one test for each of the other 4 methods. As you write the tests, you might find some refactoring opportunities; update the user class with any refactors you deem necessary and add a comment describing why you made that refactor. The only rule is that all the existing methods must still exist when you are finished!
+*/
+using UserSpace;
+
 namespace UserTests
 {
-    public class UserTest
-    {
-        [Fact]
-        public void IsCreated()
-        {
+	public class UserTest
+	{
+		[Fact]
+		public void Test_User_UserConstructor_IsCreated()
+		{
+			User joe = new User("Joe", "centeno.joe@gmail.com"); // Make new user object
 
+			Assert.Equal("Joe", joe.Name);                       // Comparing Name
+			Assert.Equal("centeno.joe@gmail.com", joe.Email);    // Comparing Email
+		}
+
+		[Fact]
+		public void Test_User_SetUp_UserIsSetUp()
+		{
+			User joe = new User("Joe", "centeno.joe@gmail.com"); // Make new user object
+            joe.CreatePassword(joe.Email, "SJB123");             // Create password
+
+            bool expectedValue = true;                           // Expected Value to test
+
+            Assert.Equal(expectedValue, joe.IsSetupComplete());  // Testing if password was created
         }
-    }
+
+		[Fact]
+		public void Test_User_CreatePassword_PasswordIsCreated()
+		{
+			User joe = new User("Joe", "centeno.joe@gmail.com");				  // Make new user object
+
+			string expectedValue = "Password Created";							  // Expected Value to test
+
+            Assert.Equal(expectedValue, joe.CreatePassword(joe.Email, "SJB123")); // Testing if password was created
+		}
+
+		[Fact]
+		public void Test_User_LogIn_UserIsAbleToLogIn()
+		{
+			User joe = new User("Joe", "centeno.joe@gmail.com"); // Make new user object
+			joe.CreatePassword(joe.Email, "SJB123");             // Create password for user
+
+            string expectedValue = "Logged In";                  // Expected Value to test
+
+            Assert.Equal(expectedValue, joe.LogIn("SJB123"));    // Testing that user is able to log in
+            Assert.True(joe.IsLoggedIn);						 // Testing LogOut() Bool propertie
+        }
+
+		[Fact]
+		public void Test_User_LogOut_UserIsAbleToLogOut()
+		{
+			User joe = new User("Joe", "centeno.joe@gmail.com");			// Make new user object
+			string expectedValue = "Please Login first before logging out"; // Expected Value to test
+
+			joe.CreatePassword(joe.Email, "SJB123");						// Create password for user
+
+			Assert.Equal(expectedValue, joe.LogOut());						// Testing Failed login attempt
+           
+            joe.LogIn("SJB123");											// Logging in using user password
+
+            Assert.True(joe.IsLoggedIn);                                    // Testing LogOut() bool
+            expectedValue = "Logged Out";                                   // Expected Value to test
+
+            Assert.Equal(expectedValue, joe.LogOut());						// Testing LogOut() method working
+            Assert.False(joe.IsLoggedIn);									// Testing LogOut() Bool propertie
+        }
+	}
 }
